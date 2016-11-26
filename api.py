e7db6995076760ad2680db797f08be88ed58b3e0
Jessica Y <cicax77@gmail.com>
2016-11-26 14:48:04 -0500

    Added basic API

--- api.py
+++ api.py
@@ -1,1 +1,15 @@
 # API
+import mysql.connector
+from twilio.rest import TwilioRestClient 
+ 
+# put your own credentials here 
+ACCOUNT_SID = "AC88e6e50d5cf1fa6e90137c29dc2b1ad5" 
+AUTH_TOKEN = "118a797d9a3a376cae56b1ab0ed34497" 
+ 
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+ 
+client.messages.create(
+    to="+19059660696", 
+    from_="+15017250604", 
+    body="Test SMS", 
+)
